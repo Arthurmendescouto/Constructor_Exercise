ba6c793158b5a522de771c5107df892090db7ed9
Arthur <arthurmendescouto16@gmail.com>
2024-12-23 14:32:13 -0300

    loading process

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,27 @@
 package src.main.java;
 
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("sdfsd");
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Enter account number: ");
+        int account=entrada.nextInt();
+        System.out.print("Enter account holder: ");
+        String holder=entrada.next();
+        System.out.println("Is there na initial deposit (y/n)?");
+        char response=entrada.next().charAt(0);
+
+        if(response=='y') {
+
+            System.out.print("Enter initial deposit value: ");
+            float balance=entrada.nextInt();
+            System.out.println("Account data:");
+            //Product product=new Product(account,holder,balance);//
+        } else if (response=='n') {
+
+        }else {
+
+        }
     }
 }
